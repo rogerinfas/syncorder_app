ad5aaad4d30d17d84d8d3b6903d1b54b6ad010f6
Roger <103950516+rogerinfas@users.noreply.github.com>
2025-06-27 16:12:48 -0500

    Host a todos

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Abierto a cualquier host
+    app.run(host='0.0.0.0', port=5000, debug=True)
